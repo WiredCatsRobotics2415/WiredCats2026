9789c040d796c9f1558c32b986b90a8380e53951
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2026-03-13 17:02:31 -0400

    ball arc added to sim

--- src/main/java/frc/visualization/BallSim.java
+++ src/main/java/frc/visualization/BallSim.java
@@ -1,9 +1,15 @@
 package frc.visualization;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.Timer;
+import frc.subsystems.drive.CommandSwerveDrivetrain;
+import frc.subsystems.shooter.Shooter;
+
+import org.littletonrobotics.junction.Logger;
+import java.util.List;
 
 public class BallSim {
 
@@ -23,11 +29,15 @@ public class BallSim {
     private Translation3d pos = new Translation3d();
     private Translation3d vel = new Translation3d();
     private double lastT = 0.0;
+    private List<Pose3d> arc = new java.util.ArrayList<>();
 
     /** Start a throw from an initial pose, with roll/pitch/yaw and a launch speed (m/s). */
     public void throwBall(Pose3d startPose, Rotation3d rpy, double speedMps) {
       // Launch direction is robot +X rotated by the given RPY (roll doesn’t matter for a sphere, but harmless).
       Translation3d dir = new Translation3d(1, 0, 0).rotateBy(rpy);
+
+      //reset arc to have nothing
+      arc = new java.util.ArrayList<>();
 
       pos = startPose.getTranslation();
       vel = new Translation3d(dir.getX() * speedMps, dir.getY() * speedMps, dir.getZ() * speedMps);
@@ -54,6 +64,11 @@ public class BallSim {
           pos.getY() + vel.getY() * dt,
           pos.getZ() + vel.getZ() * dt
       );
+
+      arc.add(new Pose3d(pos, new Rotation3d()));
+      Pose3d[] send_arc = arc.toArray(new Pose3d[0]);
+
+      Logger.recordOutput("Shooter/ball_vector", send_arc);
 
       // Stop when it hits the floor (z <= 0). Adjust if your “ground” is different.
       if (pos.getZ() <= 0.0) {
